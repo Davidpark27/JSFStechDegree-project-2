86ecaf87e87fec7fb5bc816183ec12281d626143
Davidpark27 <52177115+Davidpark27@users.noreply.github.com>
2019-07-03 14:50:18 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,27 +2,22 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
+//Global Variables. 
 
-/*** 
-Global Variables.
-***/
-const studentList = document.querySelectorAll('.student-item cf');
+console.log('hello');
+const studentList = document.querySelectorAll('.student-item');
 const studentsOnPage = 10;
 
-
-
-
 /*** 
-This is the show page function. Displays 10 students and prevents the rest from displaying.
+This is the show page function. Displays 10 students and prevents the rest from displaying 
+using a 'for loop'. 
 ***/
 const showPage = (list,page) => {
    const startIndex = (page * studentsOnPage) - studentsOnPage;
    const endIndex = (page * studentsOnPage);
-   for (let i = 0;, i < list.length; i++) {
-      if (i.= startIndex && i < endIndex) {
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
          list[i].style.display = 'block';
       } else {
          list[i].style.display = 'none';
@@ -31,16 +26,53 @@ const showPage = (list,page) => {
 
    }
 };
+//calling function to show 1st page on initial visit.
+showPage(studentList,1);
 
 
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+//function to append students on page by creating new elements.
+const appendPageLinks = (list) => {
+   const totalPages = Math.ceil(list.length/studentsOnPage);
+   const div = document.createElement('div'); 
+   div.className = 'pagination';
+   const ul = document.createElement('ul');
+   const pageDiv = document.querySelector('.page'); 
+   pageDiv.appendChild(div);
+   div.appendChild(ul) 
+      //for loop to append new 'a' and 'li' elements into 'ul'
+      for (let i = 1; i <= totalPages; i+=1) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        
+        ul.appendChild(li);
+        li.appendChild(a);
+        
+        a.textContent = i;
+        a.href = '#';
+         if (i === 1) {
+            a.className = 'active';
+         }
+         
+
+      } 
+      // The event listener that waits for users to click
+      //page number and returning showPage function with 
+      //event parameters.
+      ul.addEventListener('click', (event) => {
+         const click = event.target;
+         const pageNum = event.target.textContent;
+         showPage(studentList, pageNum);
+         const links = document.querySelectorAll('a');
+      for (let i = 0; i < links.length; i+=1) {
+          links[i].className = 'none';
+         } 
+      click.className = 'active';
+
+      }
+);
+}
+//calling the appendPageLinks function.
+appendPageLinks(studentList);
 
 
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
